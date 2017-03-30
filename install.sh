1890a18fc113ee684b21ab3ff6ebdad6d691093a
Christian Di Lorenzo <rcddeveloper@icloud.com>
2017-03-30 09:46:44 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,25 @@
+if [ -f ~/.vimrc ]; then
+    echo "Vim configuration already found. Please remove your .vimrc from your home directory and re-run."
+    exit 1
+fi
+
 mkdir ~/vimfiles
-git clone git@github.com:rcdilorenzo/vimfiles.git
+echo "Downloading vimfiles..."
+git clone git@github.com:rcdilorenzo/vimfiles.git --quiet
 ln -s ~/vimfiles ~/.vim
 ln -s ~/vimfiles/rc ~/.vimrc
 mkdir ~/vimfiles/bundle
-git clone git@github.com:gmarik/Vundle.vim.git ~/vimfiles/bundle/vundle
-echo "Open vim (ignore warning about wombat256mod if using gvim or macvim)"
+
+echo "Downloading vundle for dependencies..."
+git clone git@github.com:gmarik/Vundle.vim.git ~/vimfiles/bundle/vundle --quiet
+
+echo "Open vim (ignore warnings about missing dependencies)"
 echo "Run :BundleInstall"
 echo "Close and reopen vim"
-read -p "Once finished, press ENTER to download and install the patched fonts (or Ctrl-C to exit):"
+read -rsp $'Press any key to install the patched fonts (or Ctrl-C to exit)...\n' -n1 key
 
-echo "Installing patched fonts"
+echo "Downloading patched fonts..."
 git clone git@github.com:powerline/fonts.git
+echo "Installing patched fonts..."
 ./fonts/install.sh
 rm -rf fonts/
