db089edf10e231c3abec44a3029b468858abe189
Christian Di Lorenzo <rcddeveloper@icloud.com>
2017-03-30 10:05:59 -0400

    Improve installation (#2)
    * Add automated instructions and a screenshot

--- README.markdown
+++ README.markdown
@@ -1,12 +1,26 @@
+## Screenshot
+
+<img width="1055" alt="Screenshot at 2017-03-30" src="https://cloud.githubusercontent.com/assets/634167/24508017/2f5f4076-1530-11e7-9a38-346e93f84919.png">
+
 ## Installation
 
+```
+curl -L https://git.io/vSZX5 | bash -s
+```
+
+Note that you should always read the bash script you're about to download and run. I'm not malicious but that does not mean everyone else is nice. :-)
+
+Essentially this script just grabs the latest version of the `install.sh` file and runs it. Feel free to take a look at it [here](./install.sh).
+
+## Manual Install
+
 0. `$ mkdir ~/vimfiles`
-0. `$ git clone git@github.com:rcdilorenzo/vimfiles.git`
+0. `$ git clone git@github.com:rcdilorenzo/vimfiles.git ~/vimfiles`
 0. `$ ln -s ~/vimfiles ~/.vim`
 0. `$ ln -s ~/vimfiles/rc ~/.vimrc`
 0. `$ mkdir ~/vimfiles/bundle`
 0. `$ git clone git@github.com:gmarik/Vundle.vim.git ~/vimfiles/bundle/vundle`
-0. Open vim (ignore warning about wombat256mod if using gvim or macvim)
+0. Open vim (ignore warnings)
 0. Run `:BundleInstall`
 0. Close and reopen vim
 0. Install patched fonts (for arrows and other decorations)
---
+++ install.sh
@@ -0,0 +1,31 @@
+if [ -f ~/.vimrc ]; then
+    echo "Vim configuration already found. Please remove your .vimrc from your home directory and re-run."
+    exit 1
+fi
+
+mkdir ~/vimfiles
+echo "Downloading vimfiles..."
+git clone git@github.com:rcdilorenzo/vimfiles.git --quiet
+ln -s ~/vimfiles ~/.vim
+ln -s ~/vimfiles/rc ~/.vimrc
+mkdir ~/vimfiles/bundle
+
+echo "Downloading vundle for dependencies..."
+git clone git@github.com:gmarik/Vundle.vim.git ~/vimfiles/bundle/vundle --quiet
+
+read -n1 -r -p $'Press space to use patched fonts or any other key to continue...\n' key
+if [ "$key" = '' ]; then
+    echo "Downloading patched fonts..."
+    git clone git@github.com:powerline/fonts.git --quiet
+    echo "Installing patched fonts..."
+    ./fonts/install.sh
+    rm -rf fonts/
+fi
+
+echo "The configuration files are now all in place."
+echo "Follow these instructions to complete the installation:"
+echo "1. Open vim (ignore warnings about missing dependencies)"
+echo "2. Run :BundleInstall"
+echo "3. Close and reopen vim"
+echo ""
+echo $'Your vim setup is complete. Thanks for using my vimfiles. - @rcdilorenzo (https://git.io/vMjZB)'
